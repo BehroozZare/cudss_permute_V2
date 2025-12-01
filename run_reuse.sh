5a341a30d676d05e4bfc06dd15c0bfe8831f6a5d
Behrooz <bzareb01@gmail.com>
2025-11-30 19:28:55 -0500

    some clean up

--- run_reuse.sh
+++ run_reuse.sh
@@ -2,8 +2,4 @@
 # cmake --build build --target reuse
 ./build/bin/reuse data/nefertiti.mtx output/perm.txt output/elim_tree.txt
 
-./build/bin/reuse data/nefertiti.mtx output_parth/perm_cudss.txt output_parth/elim_tree_cudss.txt
-
-./build/bin/reuse data/nefertiti.mtx output_parth/perm_post_order.txt output_parth/elim_tree_post_order.txt
-
-./build/bin/reuse data/nefertiti.mtx output_parth/perm_reverse.txt output_parth/elim_tree_reverse.txt
+./build/bin/reuse data/nefertiti.mtx output_parth/user_defined_perm.txt output_parth/user_defined_etree.txt
